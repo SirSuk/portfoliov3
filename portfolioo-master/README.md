4266c2d011132c1767043c338a68b1743aa0822c
Radif El noun <SIRIOSUKUR@GMAIL.COM>
2023-01-01 17:05:32 +0100

    firstcommit

---
+++ portfolioo-master/README.md
@@ -0,0 +1,6 @@
+# Portfolio responsivo con HTML y CSS
+---
+## Realizado para poner en práctica conceptos y realizar pruebas de código
+Este porfolio será corregido poco a poco y readaptado a nuevas ideas
+ ![image](https://user-images.githubusercontent.com/102561036/205487109-b2e667e5-7f16-46eb-b6a1-7380f1c8bc07.png)
+
---
+++ portfolioo-master/script.js
@@ -0,0 +1,43 @@
+
+const btn= document.getElementById("btnColores");
+const domH1 = document.querySelector('.titulo_movimiento');
+const text = "Hi, I'm Radif"
+const hamburguer = document.querySelector('#burguer');
+const cambioModo = document.querySelector('.cambiomodo');
+
+
+cambioModo.addEventListener('mouseover', () => {
+    cambioModo.src = "./todoapp-modonoche.jpg"
+})
+
+cambioModo.addEventListener('mouseleave', () => {
+    cambioModo.src = "./todoapp-mododia.jpg"
+})
+
+/* handle hamburguer */
+
+
+
+
+/*background changer */
+
+btn.addEventListener('click', () => {
+    document.body.style.background = colorChange()
+})
+
+function colorChange () {
+    return `hsl(${Math.floor(Math.random()*360)}, 20% ,70%)`
+}
+
+
+/* auto text writer */
+
+let index = 0;
+
+function writeText() {
+    domH1.innerHTML = text.slice(0, index);
+    index++;
+}
+
+setInterval(writeText, 400)
+
